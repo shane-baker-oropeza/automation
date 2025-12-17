860b76b1c96d4c3141934cae4f4511ce652da248
shane-baker-oropeza <srbo1975@gmail.com>
2025-12-17 08:41:51 -0600

    Update toggle-cipher-suites.ps1

--- toggle-cipher-suites.ps1
+++ toggle-cipher-suites.ps1
@@ -3,19 +3,19 @@
     This PowerShell script ensures that the maximum size of the Windows Application event log is at least 32768 KB (32 MB).
 
 .NOTES
-    Author          : Josh Madakor
-    LinkedIn        : linkedin.com/in/joshmadakor/
-    GitHub          : github.com/joshmadakor1
-    Date Created    : 2024-09-09
-    Last Modified   : 2024-09-09
+    Author          : Shane Baker-Oropeza
+    LinkedIn        : linkedin.com/in/shane-baker-oropeza/
+    GitHub          : github.com/shane-baker-oropeza
+    Date Created    : 2025-11-06
+    Last Modified   : 2025-11-06
     Version         : 1.0
     CVEs            : N/A
     Plugin IDs      : N/A
     STIG-ID         : WN10-AU-000500
 
 .TESTED ON
-    Date(s) Tested  : 
-    Tested By       : 
+    Date(s) Tested  : 2025-11-06
+    Tested By       : Shane Baker-Oropeza
     Systems Tested  : 
     PowerShell Ver. : 
 
