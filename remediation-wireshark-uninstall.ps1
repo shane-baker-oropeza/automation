f0b34b9642dd9f21aea4fa0a1d2e5e054508c352
shane-baker-oropeza <srbo1975@gmail.com>
2025-12-17 08:44:12 -0600

    Update remediation-wireshark-uninstall.ps1

--- remediation-wireshark-uninstall.ps1
+++ remediation-wireshark-uninstall.ps1
@@ -6,14 +6,14 @@
     Make sure to run as Administrator or with appropriate privileges.
 
 .NOTES
-    Author        : Josh Madakor
-    Date Created  : 2024-09-09
-    Last Modified : 2024-09-09
+    Author        : Shane Baker-Oropeza
+    Date Created  : 2025-11-06
+    Last Modified : 2025-11-06
     Version       : 1.0
 
 .TESTED ON
-    Date(s) Tested  : 2024-09-09
-    Tested By       : Josh Madakor
+    Date(s) Tested  : 2025-11-06
+    Tested By       : Shane Baker-Oropeza
     Systems Tested  : Windows Server 2019 Datacenter, Build 1809
     PowerShell Ver. : 5.1.17763.6189
     Wireshark Ver.  : 2.2.1 (v2.2.1-0-ga6fbd27 from master-2.2)
